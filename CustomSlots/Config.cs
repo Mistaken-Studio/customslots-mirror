8afda848425f345542aeed43df9ae3ddf31313a6
Xname <xnameserver@o2.pl>
2022-10-02 20:19:32 +0200

    Delete useless option

--- CustomSlots/Config.cs
+++ CustomSlots/Config.cs
@@ -9,25 +9,13 @@ using Mistaken.Updater.Config;
 
 namespace Mistaken.CustomSlots
 {
-    /// <inheritdoc/>
     internal class Config : IAutoUpdatableConfig
     {
-        /// <inheritdoc/>
         public bool IsEnabled { get; set; } = true;
 
-        /// <summary>
-        /// Gets or sets a value indicating whether debug should be displayed.
-        /// </summary>
         [Description("If true then debug will be displayed")]
         public bool VerboseOutput { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether custom slots module is enabled.
-        /// </summary>
-        [Description("If true then custom slots module will be enabled")]
-        public bool CustomSlotsEnabled { get; set; } = false;
-
-        /// <inheritdoc/>
         [Description("Auto Update Settings")]
         public System.Collections.Generic.Dictionary<string, string> AutoUpdateConfig { get; set; }
     }
